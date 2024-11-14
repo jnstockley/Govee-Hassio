b5a666d8375fbbb2ef05819e4ea38b7e7d13abeb
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2024-11-13 19:21:26 -0600

    Improve logging

--- custom_components/govee/devices/GoveeAPIUtil.py
+++ custom_components/govee/devices/GoveeAPIUtil.py
@@ -30,14 +30,18 @@ async def get_device_state(api_key: str, sku: str, device: str, hass):
 
 
 async def control_device(api_key: str, sku: str, device: str, capability: dict, hass) -> bool:
-    headers = {'Govee-API-Key': api_key, 'Content-Type': 'application/json'}
-    body = {"requestId": "uuid", "payload": {"sku": sku, "device": device, "capability": capability}}
+    try:
+        headers = {'Govee-API-Key': api_key, 'Content-Type': 'application/json'}
+        body = {"requestId": "uuid", "payload": {"sku": sku, "device": device, "capability": capability}}
 
-    async with homeassistant.helpers.httpx_client.get_async_client(hass) as client:
+        async with homeassistant.helpers.httpx_client.get_async_client(hass) as client:
 
-        response = await client.post(f"{BASE_URL}/router/api/v1/device/control", headers=headers, json=body)
+            response = await client.post(f"{BASE_URL}/router/api/v1/device/control", headers=headers, json=body)
 
-    if response.status_code != 200:
-        log.warning(f"Failed to get device state: {response.status_code} - {response.text}")
+        if response.status_code != 200:
+            log.warning(f"Failed to get device state: {response.status_code} - {response.text}")
 
-    return response.status_code == 200
+        return response.status_code == 200
+    except Exception as e:
+        log.error(f"Failed to get device state: {e}")
+        raise e
